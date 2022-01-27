8da9d019a38f85a60234d2db0d8a7881e5433f59
skriukaite <skriukaite@gmail.com>
2022-01-27 10:10:42 +0200

    Update styling of TimeForSection

--- src/Components/TimeForSection.js
+++ src/Components/TimeForSection.js
@@ -7,19 +7,15 @@ export default function TimeForSection() {
   return (
     <div className="TimeForSection">
       <div className="section-current-temperature">
-        <div className="row">
-          <div className="col-5">
-            <img
-              src={tea}
-              alt="tea icon"
-              className="time-for-icon"
-              id="offer-image"
-            />
-            <p className="time-for">
-              Time for <span id="offer">herbal tea</span>{" "}
-            </p>
-          </div>
-        </div>
+        <img
+          src={tea}
+          alt="tea icon"
+          className="time-for-icon"
+          id="offer-image"
+        />
+        <p className="time-for">
+          Time for a <span id="offer">herbal tea</span>{" "}
+        </p>
       </div>
     </div>
   );
